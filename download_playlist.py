94f12846460b018d3443b773d2bf2eca08b4e293
mohamed ali <83076587+djv-mo@users.noreply.github.com>
2023-01-20 21:35:10 +0200

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -1,56 +1,74 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter
 from pydantic import HttpUrl
-from pytube import Playlist, YouTube
+import youtube_dl
 
 download_playlist_router = APIRouter()
 
 
-@download_playlist_router.get("/playlist/{url:path}")
-async def download_playlist(url: HttpUrl, format: str = Query(..., title='playlist Format', description='Choose the format of the playlist',
-                                                              choices=["144p", "360p", "720p", "1080p", 'audio'])):
+@download_playlist_router.get("/playlist")
+async def download_playlist(url: HttpUrl, resolution: str):
     # getting the videos of the playlist
-    yt = Playlist(url)
-    playlist_videos = []
-    for i in yt.videos:
-        playlist_videos.append(i.watch_url)
+    try:
+        ydl_opts = {}
+        if resolution == 'audio':
+            ydl_opts = {
+                'format': 'bestaudio',
+            }
+        elif resolution == 'mp4':
+            ydl_opts = {
+                "format": "best",
+            }
+        else:
+            return {'error': 'resolution should be audio or mp4'}
 
-    video_data = []
-    # stock for resolutions
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            result = ydl.extract_info(url, download=False)
+            playlist_videos = result['entries']
+            video_data = []
 
-    def video_stock(res):
-        for video in playlist_videos:
-            ytv = YouTube(video)
-            title = ytv.title
-            thumbnail = ytv.thumbnail_url
-            duration = ytv.length
-            mp4_streams = ytv.streams.filter(
-                progressive=True, resolution=res + 'p')
-            mp4 = {}
-            for stream in mp4_streams:
-                mp4[stream.itag] = {'resolution': stream.resolution,
-                                    'size_in_mb': stream.filesize_mb,
-                                    'download_url': stream.url}
-            video_data.append(
-                {"title": title, 'thumbnail': thumbnail, 'duration': duration, 'streams': mp4})
-    if format == '144p':
-        video_stock(str(144))
-    elif format == '360p':
-        video_stock(str(360))
-    elif format == '720p':
-        video_stock(str(720))
-    elif format == '1080p':
-        video_stock(str(1080))
-    elif format == 'audio':
-        for video in playlist_videos:
-            ytv = YouTube(video)
-            title = ytv.title
-            thumbnail = ytv.thumbnail_url
-            duration = ytv.length
-            mp3_streams = ytv.streams.get_audio_only()
-            mp3 = {'abr': mp3_streams.abr,
-                   'size_in_mb': mp3_streams.filesize_mb,
-                   'audio_codec': mp3_streams.audio_codec.split(".")[0],
-                   'download_url': mp3_streams.url}
-            video_data.append(
-                {"title": title, 'thumbnail': thumbnail, 'duration': duration, 'streams': mp3})
-    return video_data
+            if resolution == 'audio':
+
+                for video in playlist_videos:
+                    audio_formats = [
+                        f for f in video['formats'] if f['vcodec'] == 'none']
+                    best_audio_format = max(
+                        audio_formats, key=lambda f: f['filesize'])
+
+                    video_info = {'title': video['title'], 'thumbnail': video['thumbnail'],
+                                  'duration_sec': video['duration'], 'channel_name': video['uploader'],
+                                  'quality': 'best', 'size_in_mb': best_audio_format['filesize'] / (1024 * 1024),
+                                  'media_type': best_audio_format['ext'], 'url': best_audio_format['url']}
+
+                    video_data.append(video_info)
+                return {'data': video_data}
+            elif resolution == 'mp4':
+                for video in playlist_videos:
+                    best_formats = []
+                    for fmt in video['formats']:
+                        if fmt['vcodec'] != 'none' and fmt['acodec'] != 'none':
+                            if fmt['filesize'] is None:
+                                for similar_fmt in video['formats']:
+                                    if similar_fmt['format_note'] == fmt['format_note'] and similar_fmt['height'] <= fmt['height'] and similar_fmt['filesize'] is not None:
+                                        fmt['filesize'] = similar_fmt['filesize']
+                            if resolution in fmt['format_note'] and fmt['filesize'] is not None:
+                                if resolution == '360p':
+                                    for fmt_ in result['formats']:
+                                        if resolution == '720p' and fmt_['filesize'] > fmt['filesize']:
+                                            break
+                            new_format = {
+                                'title': video['title'],
+                                'thumbnail': video['thumbnail'],
+                                'duration_sec': video['duration'],
+                                'channel_name': video['uploader'],
+                                'quality': fmt['format_note'],
+                                'size_in_mb': fmt['filesize'] / (1024 * 1024),
+                                'media_type': fmt['ext'],
+                                'url': fmt['url']
+                            }
+                            best_formats.append(new_format)
+                    video_data.append(best_formats)
+                return {'data': video_data}
+            else:
+                return {"error": "Format not available"}
+    except:
+        return {"error": "check the url"}
