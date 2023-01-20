4814c38b42a06664a17aae59642e95d991d153b2
mohamed ali <83076587+djv-mo@users.noreply.github.com>
2023-01-20 21:34:44 +0200

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -1,34 +1,74 @@
-from fastapi import APIRouter, Query
-from pydantic import HttpUrl
-from pytube import YouTube
-
+from fastapi import APIRouter
+import youtube_dl
 download_video_router = APIRouter()
 
 
-@download_video_router.get("/download/{url:path}")
-async def download_video(url: HttpUrl, format: str = Query(..., title='Video Format', description='Choose the format of the video',
-                                                           choices=["mp3", "mp4"])):
-    yt = YouTube(url)
-    title = yt.title
-    thumbnail = yt.thumbnail_url
-    duration = yt.length
-    if format == 'mp4':
-        mp4_streams = yt.streams.filter(progressive=True)
-        mp4 = {}
-        for stream in mp4_streams:
-            mp4[stream.itag] = {'resolution': stream.resolution,
-                                'size_in_mb': stream.filesize_mb,
-                                'download_url': stream.url}
+@download_video_router.get("/video")
+async def download_video(video_id: str, resolution: str):
+    url = f"https://www.youtube.com/watch?v={video_id}"
+    try:
+        ydl_opts = {}
+        if resolution == 'audio':
+            ydl_opts = {
+                'format': 'bestaudio',
+                # 'no-m3u8-prefer-native': True,
+            }
+        elif resolution == 'mp4':
+            ydl_opts = {
+                "format": "best",
+            }
+        else:
+            return {'error': 'resolution should be audio or mp4'}
 
-        return {"title": title, 'thumbnail': thumbnail,
-                'duration': duration, 'streams': mp4}
-    elif format == 'mp3':
-        mp3_streams = yt.streams.get_audio_only()
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 
-        mp3 = {'abr': mp3_streams.abr,
-               'size_in_mb': mp3_streams.filesize_mb,
-               'audio_codec': mp3_streams.audio_codec.split(".")[0],
-               'download_url': mp3_streams.url}
+            result = ydl.extract_info(url, download=False)
 
-        return {"title": title, 'thumbnail': thumbnail,
-                'duration': duration, 'streams': mp3}
+            if resolution == 'audio':
+                audio_formats = [f for f in result['formats']
+                                 if f['vcodec'] == 'none']
+                best_audio_format = max(
+                    audio_formats, key=lambda f: f['filesize'])
+                return {'title': result['title'],
+                        'thumbnail': result['thumbnail'],
+                        'duration_sec': result['duration'],
+                        'channel_name': result['uploader'],
+                        'quality': 'best',
+                        'size_in_mb': best_audio_format['filesize'] / (1024 * 1024),
+                        'media_type': best_audio_format['ext'],
+                        'url': best_audio_format['url']}
+
+            elif resolution == 'mp4':
+                best_formats = []
+                for fmt in result['formats']:
+
+                    if fmt['vcodec'] != 'none' and fmt['acodec'] != 'none':
+                        if fmt['filesize'] is None:
+                            for similar_fmt in result['formats']:
+                                if similar_fmt['format_note'] == fmt['format_note']\
+                                        and similar_fmt['height'] <= fmt['height']\
+                                        and similar_fmt['filesize'] is not None:
+                                    fmt['filesize'] = similar_fmt['filesize']
+                        if resolution in fmt['format_note'] and fmt['filesize'] is not None:
+                            if resolution == '360p':
+                                for fmt_ in result['formats']:
+                                    if resolution == '720p' and fmt_['filesize'] > fmt['filesize']:
+                                        break
+                        new_format = {
+                            'quality': fmt['format_note'],
+                            'size_in_mb': fmt['filesize'] / (1024 * 1024),
+                            'media_type': fmt['ext'],
+                            'url': fmt['url']
+                        }
+                        best_formats.append(new_format)
+                if best_formats:
+                    return {'title': result['title'],
+                            'thumbnail': result['thumbnail'],
+                            'duration_sec': result['duration'],
+                            'channel_name': result['uploader'],
+                            'formats': best_formats}
+
+                else:
+                    return {"error": "Format not available"}
+    except:
+        return {"error": "check the url make sure you use  video id"}
