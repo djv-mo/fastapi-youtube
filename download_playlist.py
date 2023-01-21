9f37e82c66073d4db554aaab1c7cc1b34c5d32ba
mohamed ali <83076587+djv-mo@users.noreply.github.com>
2023-01-21 23:01:36 +0200

    Update download_playlist.py

--- download_playlist.py
+++ download_playlist.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import HttpUrl
 import youtube_dl
 
@@ -7,6 +7,9 @@ download_playlist_router = APIRouter()
 
 @download_playlist_router.get("/playlist")
 async def download_playlist(url: HttpUrl, resolution: str):
+    if resolution not in ['audio', 'mp4']:
+        raise HTTPException(
+            status_code=400, detail="resolution should be audio or mp4")
     # getting the videos of the playlist
     try:
         ydl_opts = {}
@@ -18,8 +21,6 @@ async def download_playlist(url: HttpUrl, resolution: str):
             ydl_opts = {
                 "format": "best",
             }
-        else:
-            return {'error': 'resolution should be audio or mp4'}
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             result = ydl.extract_info(url, download=False)
@@ -36,7 +37,7 @@ async def download_playlist(url: HttpUrl, resolution: str):
 
                     video_info = {'title': video['title'], 'thumbnail': video['thumbnail'],
                                   'duration_sec': video['duration'], 'channel_name': video['uploader'],
-                                  'quality': 'best', 'size_in_mb': best_audio_format['filesize'] / (1024 * 1024),
+                                  'quality': 'best', 'size_in_mb': best_audio_format['filesize'] / (1024 * 1024) if best_audio_format['filesize'] is not None else None,
                                   'media_type': best_audio_format['ext'], 'url': best_audio_format['url']}
 
                     video_data.append(video_info)
@@ -65,7 +66,7 @@ async def download_playlist(url: HttpUrl, resolution: str):
                             new_format = {
 
                                 'quality': fmt['format_note'],
-                                'size_in_mb': fmt['filesize'] / (1024 * 1024),
+                                'size_in_mb': fmt['filesize'] / (1024 * 1024) if fmt['filesize'] is not None else None,
                                 'media_type': fmt['ext'],
                                 'url': fmt['url']
                             }
@@ -73,6 +74,11 @@ async def download_playlist(url: HttpUrl, resolution: str):
                     video_data.append(video_info)
                 return {'data': video_data}
             else:
-                return {"error": "Format not available"}
-    except:
-        return {"error": "check the url"}
+                raise HTTPException(
+                    status_code=404, detail="Format not available")
+    except youtube_dl.utils.DownloadError as e:
+        raise HTTPException(
+            status_code=404, detail="Invalid video id or url or video is deleted from YouTube")
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"An error occurred, please try again later {e}")
