94975104fa759f8a8e882959595f14776a3c408a
mohamed ali <83076587+djv-mo@users.noreply.github.com>
2023-01-21 15:07:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from download_video import download_video_router
 from download_playlist import download_playlist_router
+# from StreamDownload import download_stream_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(download_video_router)
 app.include_router(download_playlist_router)
+# app.include_router(download_stream_router)
